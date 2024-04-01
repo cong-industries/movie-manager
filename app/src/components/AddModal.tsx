6d95af2f8b2d4eb1c43a24f523febed96e819c8a
congnguyen <c.nguyen@r-ie.de>
2024-04-01 23:33:59 +0200

    Add regisseur field to movie data and update AddModal component

--- app/src/components/AddModal.tsx
+++ app/src/components/AddModal.tsx
@@ -1,12 +1,13 @@
 // src/components/Card.tsx
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
-const AddModal = ({closeModal, submitAddModal, addModalData}) => {
+const AddModal = ({ closeModal, submitAddModal, addModalData }) => {
     const [newName, setnewName] = useState(addModalData.name)
     const [newImageUrl, setNewImageUrl] = useState(addModalData.imageUrl)
     const [newGenre, setNewGenre] = useState(addModalData.genre)
     const [newDescription, setNewDescription] = useState(addModalData.description)
+    const [newRegisseur, setNewRegisseur] = useState(addModalData.regisseur)
 
     return (
         <div className="editModal">
@@ -16,24 +17,28 @@ const AddModal = ({closeModal, submitAddModal, addModalData}) => {
                 <div>
                     <div className="inputWrapper">
                         <label htmlFor="newName">Name</label>
-                        <input name="newName" defaultValue={newName} onChange={value => setnewName(value.target.value)}/>
+                        <input name="newName" defaultValue={newName} onChange={value => setnewName(value.target.value)} />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="newImageurl">Bildurl</label>
-                        <input name="newImageurl" defaultValue={newImageUrl} onChange={value => setNewImageUrl(value.target.value)}/>
+                        <input name="newImageurl" defaultValue={newImageUrl} onChange={value => setNewImageUrl(value.target.value)} />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="newGenre">Genre</label>
-                        <input name="newGenre" defaultValue={newGenre} onChange={value => setNewGenre(value.target.value)}/>
+                        <input name="newGenre" defaultValue={newGenre} onChange={value => setNewGenre(value.target.value)} />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="newDescription">Beschreibung</label>
-                        <input name="newDescription" defaultValue={newDescription} onChange={value => setNewDescription(value.target.value)}/>
+                        <input name="newDescription" defaultValue={newDescription} onChange={value => setNewDescription(value.target.value)} />
+                    </div>
+                    <div className="inputWrapper">
+                        <label htmlFor="newRegisseur">Regisseur</label>
+                        <input name="newRegisseur" defaultValue={addModalData.regisseur} onChange={value => setNewRegisseur(value.target.value)} />
                     </div>
                 </div>
 
                 <Button onClick={() => {
-                    submitAddModal(newName, newImageUrl, newGenre, newDescription)
+                    submitAddModal(newName, newImageUrl, newGenre, newDescription, newRegisseur)
                     closeModal()
                 }} color="#8CD57A">Hinzufügen</Button>
             </div>
